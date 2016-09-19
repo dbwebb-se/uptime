4c3a91ce5a9117b2f9d9f335e15aa128089c1fa8
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-09-19 15:24:02 +0200

    update enddate

--- htdocs/config_sample.php
+++ htdocs/config_sample.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);     // Display all errors
 
 // When did tournament start?
 define("TOURNAMENT_START", "2016-09-17");
-define("TOURNAMENT_END", "2016-05-19");
+define("TOURNAMENT_END", "2017-05-19");
 
 // Database conneection details
 define("DSN", "sqlite:" . __DIR__ . "/../data/uptime.sqlite");
